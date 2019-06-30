e472fcd5e73063ea057cd06ec366f6e35c4ab0ba
Pearl Dsilva <pearl1594@gmail.com>
2019-06-30 22:24:17 +0530

    Javascript for handling listing of posts and creating posts

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function listAllPosts(){
+    window.location.href = "./html/bloglist.html"
+}
+
+function createPost(){
+    var modal = document.getElementById("createPostModal");
+    modal.style.display = "block";
+}
